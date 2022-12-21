a2702fbc6ac818867278df220f7a03d2635f88c4
Samapti Rudra <samapti.rudra@nagarro.com>
2022-12-21 20:25:20 +0530

    add java file

---
+++ Example.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int[] marks = new int[3];
+        //int marks[]= new int[3];(another way to declare array)
+        marks[0]=90;//phy;
+        marks[1]=95;//math
+        marks[2]=80;//chem
+        for(int i =0;i<3;i++) {
+        	System.out.println(marks[i]);
+        	System.out.println();
+        }
+        
+	}
+
+}
